26bad725da84e7b0cfab87f22fea030d316a10d5
Med Aziz Laafine <laafine.med.aziz@gmail.com>
2025-04-20 17:58:42 +0100

    integration: idk

--- src/app/links/transactions/transactions.component.ts
+++ src/app/links/transactions/transactions.component.ts
@@ -17,30 +17,26 @@ export class TransactionsComponent {
 
   ngOnInit(): void {
     this.loading = true;
-    const userId = localStorage.getItem('userId');
 
-    if (userId) {
-      this.transactionService.getTransactionsByUser(userId).subscribe(
-        (data) => {
-          this.transactions = data
-            .sort(
-              (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-            ) // tri décroissant
-            .slice(0, 3); // prend les 3 premières
-          this.loading = false;
-        },
-        (error) => {
-          console.error(
-            'Erreur lors de la récupération des transactions',
-            error
-          );
-          this.loading = false;
-        }
-      );
-    } else {
-      console.error('User ID non trouvé');
-      this.loading = false;
-    }
+    this.transactionService.getTransactionsByUser().subscribe(
+      (data) => {
+        console.log(data)
+        this.transactions = data
+          .sort(
+            (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+          ) // tri décroissant
+          .slice(0, 3); // prend les 3 premières
+        this.loading = false;
+      },
+      (error) => {
+        console.error(
+          'Erreur lors de la récupération des transactions',
+          error
+        );
+        this.loading = false;
+      }
+    );
+    
   }
 
   searchText = '';
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,14 +7,16 @@ import { map } from 'rxjs';
   providedIn: 'root',
 })
 export class LoginService {
-  private apiUrl = 'http://localhost:8069/web/session/authenticate';
+  private apiUrl = 'http://localhost:8069/api/web/session/authenticate';
 
   constructor(private http: HttpClient) {}
   
   // Méthode pour envoyer l'email et le mot de passe au backend
   login(login: string, password: string): Observable<any> {
     const userData = { login, password }; // Données à envoyer
-    return this.http.post(this.apiUrl, userData); // Envoi des données avec POST
+    return this.http.post(this.apiUrl, userData,{
+      withCredentials: true, 
+      }); // Envoi des données avec POST
   }
 }
 
